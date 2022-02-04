7b628a2495b0039189f3fe1f314432f850c8fb87
emilio kariuki <70817006+emilio-kariuki@users.noreply.github.com>
2022-02-04 13:40:23 +0300

    February 4, 2022 1:40 PM

--- lib/screens/welcome/body.dart
+++ lib/screens/welcome/body.dart
@@ -4,37 +4,40 @@ import 'package:authentication/Constants/colors.dart';
 import "package:flutter/material.dart";
 import 'package:flutter_svg/flutter_svg.dart';
 
-class Welcome extends StatefulWidget {
-  const Welcome({Key? key}) : super(key: key);
-
-  @override
-  State<Welcome> createState() => _WelcomeState();
-}
-
-class _WelcomeState extends State<Welcome> {
+class Welcome extends StatelessWidget {
+  final Widget child;
+  const Welcome({Key? key, required this.child}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       backgroundColor: kprimaryColor,
       body: SafeArea(
         child: Stack(
           children: [
+            child,
             Positioned(
-              top: 0,
-              left: 0,
-              child: Image.asset("assets/main_top.png",color: const Color.fromARGB(255, 24, 194, 52),height: 130)),
-              Positioned(
-              top: 0,
-              right: 0,
-              child: Image.asset("assets/main_top.png",color: const Color.fromARGB(255, 24, 194, 52),height: 130)),
-              Positioned(
-              bottom: 0,
-              left: 0,
-              child: Image.asset("assets/main_bottom.png",color: const Color.fromARGB(255, 24, 194, 52),height: 130)),
-              Positioned(
-              bottom: 0,
-              right: 0,
-              child: Image.asset("assets/login_bottom.png",color: const Color.fromARGB(255, 24, 194, 52),height: 130))
+                top: 0,
+                left: 0,
+                child: Image.asset("assets/main_top.png",
+                    color: const Color.fromARGB(255, 24, 194, 52),
+                    height: 130)),
+            Positioned(
+                top: 0,
+                right: 0,
+                child: Image.asset("assets/main_top.png",
+                    color: const Color.fromARGB(255, 24, 194, 52),
+                    height: 130)),
+            Positioned(
+                bottom: 0,
+                left: 0,
+                child: Image.asset("assets/main_bottom.png",
+                    color: const Color.fromARGB(255, 24, 194, 52),
+                    height: 130)),
+            Positioned(
+                bottom: 0,
+                right: 0,
+                child: Image.asset("assets/login_bottom.png",
+                    color: const Color.fromARGB(255, 24, 194, 52), height: 130))
           ],
         ),
       ),
