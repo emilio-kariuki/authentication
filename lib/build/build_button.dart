d384e23b27b3c204cca0050de0fc799564b259a9
emilio kariuki <70817006+emilio-kariuki@users.noreply.github.com>
2022-02-04 13:56:32 +0300

    February 4, 2022 1:56 PM

--- lib/build/build_button.dart
+++ lib/build/build_button.dart
@@ -10,7 +10,7 @@ class BuildButton extends StatelessWidget {
   Widget build(BuildContext context) {
     final mediaQuery = MediaQuery.of(context).size;
     return Padding(
-      padding: const EdgeInsets.fromLTRB(10, 0, 10,0),
+      padding: const EdgeInsets.fromLTRB(7, 0, 7,0),
       child: Container(
         height: 45,
         width: mediaQuery.width ,
