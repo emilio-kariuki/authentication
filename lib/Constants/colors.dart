df893e6ca8af6156821c2be8de44e2e4ad4d463c
emilio kariuki <70817006+emilio-kariuki@users.noreply.github.com>
2022-02-04 14:15:11 +0300

    February 4, 2022 2:15 PM

--- lib/Constants/colors.dart
+++ lib/Constants/colors.dart
@@ -6,7 +6,7 @@ const Color kaccentColor = Color.fromARGB(255, 220, 223, 228);
 
 const Color kTextColor = Color.fromARGB(255, 165, 166, 168);
 
-const Color kButtonColor = Color.fromARGB(255, 169, 170, 169);
+const Color kButtonColor = Color.fromARGB(255, 120, 117, 121);
 
 const Color kbottonColor = Color.fromRGBO(234, 78, 74, 1);
 
