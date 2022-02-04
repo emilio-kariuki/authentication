1de90e945dd4ff97c7d0f4a8db5414b4c7eca495
emilio kariuki <70817006+emilio-kariuki@users.noreply.github.com>
2022-02-04 15:54:50 +0300

    February 4, 2022 3:54 PM

---
+++ lib/build/fields/build_back.dart
@@ -0,0 +1,19 @@
+import 'package:authentication/Constants/colors.dart';
+import "package:flutter/material.dart";
+
+class BuildBack extends StatelessWidget {
+  const BuildBack({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 45,
+      height: 45,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: kprimaryColor,
+      ),
+      padding: const EdgeInsets.all(8),
+    );
+  }
+}
