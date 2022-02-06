5a4b50d38c9d5000b5968f5a6c028f3edf5aef3b
emilio kariuki <70817006+emilio-kariuki@users.noreply.github.com>
2022-02-06 19:05:24 +0300

    February 6, 2022 7:05 PM

--- lib/authentication/register.dart
+++ lib/authentication/register.dart
@@ -11,6 +11,8 @@ import "package:flutter/material.dart";
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+
+
 class Register extends StatefulWidget {
   const Register({Key? key}) : super(key: key);
 
