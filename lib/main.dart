1f3b24c9a1de11830ff0b3bb9e334fcdebe9e394
emilio kariuki <70817006+emilio-kariuki@users.noreply.github.com>
2022-02-06 18:17:41 +0300

    February 6, 2022 6:17 PM

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:authentication/screens/welcome/welcome.dart';
 import 'package:flutter/material.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MaterialApp(
      home: Welcome(),
     debugShowCheckedModeBanner: false,
