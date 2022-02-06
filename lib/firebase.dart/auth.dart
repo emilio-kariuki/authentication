72252b09d55c4ca754d708aea24c4733f5039fa9
emilio kariuki <70817006+emilio-kariuki@users.noreply.github.com>
2022-02-06 15:57:13 +0300

    February 6, 2022 3:57 PM

--- lib/firebase.dart/auth.dart
+++ lib/firebase.dart/auth.dart
@@ -1,2 +1,11 @@
 import "package:flutter/material.dart";
 
+import "package:firebase_auth/firebase_auth.dart";
+
+class Auth {
+  FirebaseAuth auth = FirebaseAuth.instance;
+
+  // Future<User> signUser() async{
+
+  // }
+}
