df5306a324d533ee623005b5d5888bb2882d7b2e
emilio kariuki <70817006+emilio-kariuki@users.noreply.github.com>
2022-02-06 19:51:59 +0300

    February 6, 2022 7:51 PM

--- lib/firebase.dart/auth.dart
+++ lib/firebase.dart/auth.dart
@@ -5,6 +5,7 @@ import "package:flutter/material.dart";
 
 import "package:firebase_auth/firebase_auth.dart";
 import 'package:google_fonts/google_fonts.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class Auth {
   FirebaseAuth auth = FirebaseAuth.instance;
@@ -68,5 +69,43 @@ class Auth {
             duration: const Duration(milliseconds: 900)));
       }
     }
+  }
+
+  static Future<User?> signInWithGoogle({required BuildContext context}) async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user;
+
+    final GoogleSignIn googleSignIn = GoogleSignIn();
+
+    final GoogleSignInAccount? googleSignInAccount =
+        await googleSignIn.signIn();
+
+    if (googleSignInAccount != null) {
+      final GoogleSignInAuthentication googleSignInAuthentication =
+          await googleSignInAccount.authentication;
+
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleSignInAuthentication.accessToken,
+        idToken: googleSignInAuthentication.idToken,
+      );
+
+      try {
+        final UserCredential userCredential =
+            await auth.signInWithCredential(credential);
+
+        user = userCredential.user;
+      } on FirebaseAuthException catch (e) {
+        if (e.code == 'account-exists-with-different-credential') {
+          // handle the error here
+        }
+        else if (e.code == 'invalid-credential') {
+          // handle the error here
+        }
+      } catch (e) {
+        // handle the error here
+      }
+    }
+
+    return user;
   }
 }
