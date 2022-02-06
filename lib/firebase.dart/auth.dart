43a29e93f0e03a61d9ab36ca7e34cb8a5e8701cd
emilio kariuki <70817006+emilio-kariuki@users.noreply.github.com>
2022-02-06 18:48:22 +0300

    February 6, 2022 6:48 PM

--- lib/firebase.dart/auth.dart
+++ lib/firebase.dart/auth.dart
@@ -10,34 +10,29 @@ class Auth {
   FirebaseAuth auth = FirebaseAuth.instance;
 
   //Register user
-  Future register(
-      String email, String password, BuildContext context) async {
+  Future register(String email, String password, BuildContext context) async {
     try {
-      UserCredential userCredential = await auth
-          .createUserWithEmailAndPassword(email: email, password: password);
+      UserCredential userCredential = await auth.createUserWithEmailAndPassword(
+          email: email, password: password);
       return userCredential;
     } on FirebaseAuthException catch (e) {
       if (e.code == 'weak-password') {
         ScaffoldMessenger.of(context).showSnackBar(SnackBar(
             elevation: 10,
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20)),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
             content: (Text("The password is to weak",
-                style: GoogleFonts.roboto(
-                    fontSize: 19, color: kaccentColor))),
-            backgroundColor:
-                const Color.fromARGB(255, 165, 123, 32),
+                style: GoogleFonts.roboto(fontSize: 19, color: kaccentColor))),
+            backgroundColor: const Color.fromARGB(255, 165, 123, 32),
             duration: const Duration(milliseconds: 900)));
       } else if (e.code == 'email-already-in-use') {
         ScaffoldMessenger.of(context).showSnackBar(SnackBar(
             elevation: 10,
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20)),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
             content: (Text("The account already exists for that email",
-                style: GoogleFonts.roboto(
-                    fontSize: 19, color: kaccentColor))),
-            backgroundColor:
-                const Color.fromARGB(255, 165, 123, 32),
+                style: GoogleFonts.roboto(fontSize: 19, color: kaccentColor))),
+            backgroundColor: const Color.fromARGB(255, 165, 123, 32),
             duration: const Duration(milliseconds: 900)));
       }
     } catch (e) {
@@ -45,39 +40,31 @@ class Auth {
     }
   }
 
-  Future login(
-      String email, String password, BuildContext context) async {
+  Future login(String email, String password, BuildContext context) async {
     try {
-      UserCredential userCredential = await auth
+      UserCredential userCredential = await FirebaseAuth.instance
           .signInWithEmailAndPassword(email: email, password: password);
       return userCredential;
     } on FirebaseAuthException catch (e) {
-      if (e.code == 'weak-password') {
+      if (e.code == 'user-not-found') {
         ScaffoldMessenger.of(context).showSnackBar(SnackBar(
             elevation: 10,
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20)),
-            content: (Text("The password is to weak",
-                style: GoogleFonts.roboto(
-                    fontSize: 19, color: kaccentColor))),
-            backgroundColor:
-                const Color.fromARGB(255, 165, 123, 32),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+            content: (Text("No user found for that email",
+                style: GoogleFonts.roboto(fontSize: 19, color: kaccentColor))),
+            backgroundColor: const Color.fromARGB(255, 165, 123, 32),
             duration: const Duration(milliseconds: 900)));
-      } else if (e.code == 'email-already-in-use') {
+      } else if (e.code == 'wrong-password') {
         ScaffoldMessenger.of(context).showSnackBar(SnackBar(
             elevation: 10,
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20)),
-            content: (Text("The account already exists for that email",
-                style: GoogleFonts.roboto(
-                    fontSize: 19, color: kaccentColor))),
-            backgroundColor:
-                const Color.fromARGB(255, 165, 123, 32),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+            content: (Text("Wrong password Try again",
+                style: GoogleFonts.roboto(fontSize: 19, color: kaccentColor))),
+            backgroundColor: const Color.fromARGB(255, 165, 123, 32),
             duration: const Duration(milliseconds: 900)));
       }
-    } catch (e) {
-      print(e);
     }
   }
-  
 }
