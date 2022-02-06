defcb37823c6354f977b3f8997e167bf5981c01b
emilio kariuki <70817006+emilio-kariuki@users.noreply.github.com>
2022-02-06 16:02:00 +0300

    February 6, 2022 4:02 PM

--- lib/firebase.dart/auth.dart
+++ lib/firebase.dart/auth.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_print
+
 import "package:flutter/material.dart";
 
 import "package:firebase_auth/firebase_auth.dart";
@@ -5,10 +7,18 @@ import "package:firebase_auth/firebase_auth.dart";
 class Auth {
   FirebaseAuth auth = FirebaseAuth.instance;
 
-  Future<UserCredential> RegisterUser(
+  Future<User?> register(
       String email, String password, String name, BuildContext context) async {
-    UserCredential userCredential = await auth.createUserWithEmailAndPassword(
-        email: email, password: password);
-    return userCredential;
+    try {
+      UserCredential userCredential = await auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      return userCredential.user;
+    } on FirebaseAuthException catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text(e.message.toString()),
+          duration: const Duration(milliseconds: 600)));
+    } catch (e) {
+      print(e);
+    }
   }
 }
