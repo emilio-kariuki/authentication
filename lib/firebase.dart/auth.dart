67386e536b2cd12a4b198acf75816047c649e0f4
emilio kariuki <70817006+emilio-kariuki@users.noreply.github.com>
2022-02-06 18:45:23 +0300

    February 6, 2022 6:45 PM

--- lib/firebase.dart/auth.dart
+++ lib/firebase.dart/auth.dart
@@ -1,8 +1,10 @@
 // ignore_for_file: avoid_print
 
+import 'package:authentication/Constants/colors.dart';
 import "package:flutter/material.dart";
 
 import "package:firebase_auth/firebase_auth.dart";
+import 'package:google_fonts/google_fonts.dart';
 
 class Auth {
   FirebaseAuth auth = FirebaseAuth.instance;
@@ -11,14 +13,32 @@ class Auth {
   Future register(
       String email, String password, BuildContext context) async {
     try {
-      UserCredential userCredential = await FirebaseAuth.instance
+      UserCredential userCredential = await auth
           .createUserWithEmailAndPassword(email: email, password: password);
       return userCredential;
     } on FirebaseAuthException catch (e) {
       if (e.code == 'weak-password') {
-        print('The password provided is too weak.');
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            elevation: 10,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20)),
+            content: (Text("The password is to weak",
+                style: GoogleFonts.roboto(
+                    fontSize: 19, color: kaccentColor))),
+            backgroundColor:
+                const Color.fromARGB(255, 165, 123, 32),
+            duration: const Duration(milliseconds: 900)));
       } else if (e.code == 'email-already-in-use') {
-        print('The account already exists for that email.');
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            elevation: 10,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20)),
+            content: (Text("The account already exists for that email",
+                style: GoogleFonts.roboto(
+                    fontSize: 19, color: kaccentColor))),
+            backgroundColor:
+                const Color.fromARGB(255, 165, 123, 32),
+            duration: const Duration(milliseconds: 900)));
       }
     } catch (e) {
       print(e);
