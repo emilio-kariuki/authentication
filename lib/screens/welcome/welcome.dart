da9ad04bd370ccf936ad940b58bab3ea1e6eece4
emilio kariuki <70817006+emilio-kariuki@users.noreply.github.com>
2022-02-06 16:27:42 +0300

    February 6, 2022 4:27 PM

--- lib/screens/welcome/welcome.dart
+++ lib/screens/welcome/welcome.dart
@@ -21,40 +21,42 @@ class _WelcomeState extends State<Welcome> {
   Widget build(BuildContext context) {
     final mediaQuery = MediaQuery.of(context).size;
     return BackGround(
-        child: Center(
-      child: Padding(
-        padding: const EdgeInsets.all(50.0),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(17, 0, 0, 0),
-              child: Text("Welcome",
-                  style: GoogleFonts.robotoCondensed(
-                      fontSize: 35, color: Colors.white)),
+        child: SingleChildScrollView(
+          child: Center(
+              child: Padding(
+          padding: const EdgeInsets.all(50.0),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(17, 0, 0, 0),
+                child: Text("Welcome",
+                    style: GoogleFonts.robotoCondensed(
+                        fontSize: 35, color: Colors.white)),
+              ),
+              SvgPicture.asset("assets/svg/chat.svg",
+                  height: mediaQuery.height * 0.4),
+              const SizedBox(height: 40),
+              CustomButton(
+                  func: () {
+                    setState(() {
+                      Navigator.of(context).pushReplacement(
+                          MaterialPageRoute(builder: (context) => Register()));
+                    });
+                  },
+                  action: "Register"),
+              const SizedBox(
+                height: 15,
+              ),
+              CustomButton(func: () {
+                setState(() {
+                      Navigator.of(context).pushReplacement(
+                          MaterialPageRoute(builder: (context) => Login()));
+                    });
+              }, action: "Login")
+            ],
+          ),
+              ),
             ),
-            SvgPicture.asset("assets/svg/chat.svg",
-                height: mediaQuery.height * 0.4),
-            const SizedBox(height: 40),
-            CustomButton(
-                func: () {
-                  setState(() {
-                    Navigator.of(context).pushReplacement(
-                        MaterialPageRoute(builder: (context) => Register()));
-                  });
-                },
-                action: "Register"),
-            const SizedBox(
-              height: 15,
-            ),
-            CustomButton(func: () {
-              setState(() {
-                    Navigator.of(context).pushReplacement(
-                        MaterialPageRoute(builder: (context) => Login()));
-                  });
-            }, action: "Login")
-          ],
-        ),
-      ),
-    ));
+        ));
   }
 }
