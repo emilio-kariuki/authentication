80ad9427327db55f32b08c2c7e6c8d5011e92ac6
emilio kariuki <70817006+emilio-kariuki@users.noreply.github.com>
2022-02-06 16:00:43 +0300

    February 6, 2022 4:00 PM

--- lib/firebase.dart/auth.dart
+++ lib/firebase.dart/auth.dart
@@ -5,7 +5,10 @@ import "package:firebase_auth/firebase_auth.dart";
 class Auth {
   FirebaseAuth auth = FirebaseAuth.instance;
 
-  // Future<User> signUser() async{
-
-  // }
+  Future<UserCredential> RegisterUser(
+      String email, String password, String name, BuildContext context) async {
+    UserCredential userCredential = await auth.createUserWithEmailAndPassword(
+        email: email, password: password);
+    return userCredential;
+  }
 }
