3c0895edb7f4a4ec62a331557baee96865a889bc
emilio kariuki <70817006+emilio-kariuki@users.noreply.github.com>
2022-02-06 14:52:09 +0300

    February 6, 2022 2:52 PM

--- lib/authentication/login.dart
+++ lib/authentication/login.dart
@@ -2,6 +2,7 @@ import 'package:authentication/Constants/colors.dart';
 import 'package:authentication/authentication/register.dart';
 import 'package:authentication/build/custom_box.dart';
 import "package:flutter/material.dart";
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -13,24 +14,26 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: kprimaryColor,
       body: SafeArea(
           child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-        children: [
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
             Row(
               // mainAxisSize: MainAxisSize.min,
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
                 Padding(
-                  padding: const EdgeInsets.only(left:16),
+                  padding: const EdgeInsets.only(left: 16),
                   child: CustomBox(
                       func: () {
                         setState(() {
                           Navigator.of(context).pushReplacement(
-                              MaterialPageRoute(builder: (context) => const Register()));
+                              MaterialPageRoute(
+                                  builder: (context) => const Register()));
                         });
                       },
                       iconUrl: 'assets/icons/register.png',
@@ -41,21 +44,22 @@ class _LoginState extends State<Login> {
             Row(
               // ignore: prefer_const_literals_to_create_immutables
               children: [
-                 Expanded(
-                  child:  Container(
-                    margin: const EdgeInsets.only(left: 15,right: 10),
-                    child: const Divider(
-                      thickness: 1,
-                      height: 50,
-                      color: kaccentColor,
-                    ),
-                  )),
+                Expanded(
+                    child: Container(
+                  margin: const EdgeInsets.only(left: 15, right: 10),
+                  child: const Divider(
+                    thickness: 1,
+                    height: 50,
+                    color: kaccentColor,
+                  ),
+                )),
               ],
             ),
-
-        ],
-      ),
-          )),
+            Center(child: SvgPicture.asset("assets/svg/login.svg",height: mediaQuery.height * 0.2,)),
+            
+          ],
+        ),
+      )),
     );
   }
 }
