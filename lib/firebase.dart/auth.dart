6e486601c9352f355009913567048984e18d15ee
emilio kariuki <70817006+emilio-kariuki@users.noreply.github.com>
2022-02-07 14:05:06 +0300

    February 7, 2022 2:05 PM

--- lib/firebase.dart/auth.dart
+++ lib/firebase.dart/auth.dart
@@ -8,7 +8,7 @@ import "package:flutter/material.dart";
 import "package:firebase_auth/firebase_auth.dart";
 import 'package:google_fonts/google_fonts.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:flutter/foundation.dart' show kIsWeb;
+
 
 class Auth {
   FirebaseAuth auth = FirebaseAuth.instance;
